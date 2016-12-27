4ef3e5b8ea6cc555e09314af6c82191932c31658
D. J. Hagberg <dhagberg@millibits.com>
2016-12-27 05:20:15 +0000

    Add cleanup if container currently/previously running

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -xe
+prevrun=$(docker inspect -f '{{.State.Running}}' archiva \
+    2>/dev/null || true)
+case "$prevrun" in
+    true)  docker stop archiva; docker rm archiva ;;
+    false) docker rm archiva ;;
+esac
 exec docker run -d --name archiva \
     -v /tank/archiva:/var/archiva:Z \
     -p 9999:9999 \
-    --restart always \
+    --restart unless-stopped \
     dhagberg/co811-archiva:latest
